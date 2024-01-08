d3af9723631ec8319c3a4c3cc3095f1a12f0919e
Xrolllex <karolkaczanowski@wp.pl>
2024-01-08 18:06:56 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { actions } from '../redux/store';
 import ContactForm from './Form';
 import ContactList from './List';
-const { addContact } = actions;
 
+const { addContact } = actions;
 
 const App = () => {
   const dispatch = useDispatch();
-    useEffect(() => {
+  const contacts = useSelector(state => state.contacts);
+
+  useEffect(() => {
     const storedContacts = localStorage.getItem('contacts');
     if (storedContacts) {
       JSON.parse(storedContacts).forEach(contact =>
@@ -17,12 +19,16 @@ const App = () => {
     }
   }, [dispatch]);
 
-    return (
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
     <div>
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
-            <ContactList />
+      <ContactList />
     </div>
   );
 };
